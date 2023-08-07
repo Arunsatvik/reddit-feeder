35c7dddb63e991d194d83e874c245f4eeb433207
Arunsatvik <satvikmpalli2000@gmail.com>
2023-08-07 09:37:23 -0700

    Level-4

--- src/components/RedditBrowser.js
+++ src/components/RedditBrowser.js
@@ -7,13 +7,38 @@ const RedditBrowser = () => {
   const [selectedSubreddit, setSelectedSubreddit] = useState(null);
   const [posts, setPosts] = useState([]);
   const [selectedPost, setSelectedPost] = useState(null);
-  const [sortOrder, setSortOrder] = useState('hot'); // Default sort order is 'hot'
+  const [sortOrder, setSortOrder] = useState('hot');
   const [inputSubreddit, setInputSubreddit] = useState('');
   const [autocompleteResults, setAutocompleteResults] = useState([]);
+  const [postHistory, setPostHistory] = useState([]);
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
+  const [currentPostIndex, setCurrentPostIndex] = useState(-1);
+
+  useEffect(() => {
+    setCanGoBack(currentPostIndex > 0);
+    setCanGoForward(currentPostIndex < postHistory.length - 1);
+  }, [currentPostIndex, postHistory]);
+
+  useEffect(() => {
+    // When a new subreddit is selected, reset the current post index and post history
+    setCurrentPostIndex(-1);
+    setPostHistory([]);
+  }, [selectedSubreddit]);
+
+  useEffect(() => {
+    if (selectedSubreddit && currentPostIndex >= 0) {
+      // When the current post index changes, update the selected post based on the post history
+      setSelectedPost(postHistory[currentPostIndex]);
+    }
+  }, [currentPostIndex, postHistory, selectedSubreddit]);
+
+
 
   useEffect(() => {
     if (inputSubreddit.length > 0) {
-      axios.get(`https://www.reddit.com/subreddits/search.json?q=${inputSubreddit}`)
+      axios
+        .get(`https://www.reddit.com/subreddits/search.json?q=${inputSubreddit}`)
         .then((response) => {
           setAutocompleteResults(response.data.data.children);
         })
@@ -21,13 +46,13 @@ const RedditBrowser = () => {
           console.error('Error fetching autocomplete results:', error);
         });
     } else {
-      setAutocompleteResults([]); // Clear autocomplete results when inputSubreddit is empty
+      setAutocompleteResults([]);
     }
   }, [inputSubreddit]);
 
   useEffect(() => {
-    // Fetch list of subreddits
-    axios.get('https://www.reddit.com/subreddits/popular.json')
+    axios
+      .get('https://www.reddit.com/subreddits/popular.json')
       .then((response) => {
         setSubreddits(response.data.data.children);
       })
@@ -38,11 +63,11 @@ const RedditBrowser = () => {
 
   useEffect(() => {
     if (selectedSubreddit) {
-      // Fetch list of posts for the selected subreddit and current sort order
-      axios.get(`https://www.reddit.com/r/${selectedSubreddit}/${sortOrder}.json`)
+      axios
+        .get(`https://www.reddit.com/r/${selectedSubreddit}/${sortOrder}.json`)
         .then((response) => {
           setPosts(response.data.data.children);
-          setSelectedPost(null); // Reset selected post when changing subreddit or sort order
+          setSelectedPost(null);
         })
         .catch((error) => {
           console.error('Error fetching posts:', error);
@@ -52,10 +77,21 @@ const RedditBrowser = () => {
 
   const handleSubredditClick = (subreddit) => {
     setSelectedSubreddit(subreddit.data.display_name);
+    setSelectedPost(null);
+    setPostHistory([]); // Reset post history when a new subreddit is selected
   };
 
   const handlePostClick = (post) => {
     setSelectedPost(post);
+    if (currentPostIndex !== postHistory.length - 1) {
+      // If the current post is not the last post in history, remove the forward history and add the new post to history
+      setPostHistory((prevHistory) => prevHistory.slice(0, currentPostIndex + 1).concat(post));
+      setCurrentPostIndex((prevIndex) => prevIndex + 1);
+    } else {
+      // If the current post is the last post in history, simply add the new post to history
+      setPostHistory((prevHistory) => [...prevHistory, post]);
+      setCurrentPostIndex((prevIndex) => prevIndex + 1);
+    }
   };
 
   const handleSortOrderChange = (event) => {
@@ -69,7 +105,19 @@ const RedditBrowser = () => {
   const handleSubredditSubmit = (event) => {
     event.preventDefault();
     setSelectedSubreddit(inputSubreddit);
-    setInputSubreddit(''); // Clear the input field after submitting
+    setInputSubreddit('');
+  };
+
+  const handleBackClick = () => {
+    if (canGoBack) {
+      setCurrentPostIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const handleForwardClick = () => {
+    if (canGoForward) {
+      setCurrentPostIndex((prevIndex) => prevIndex + 1);
+    }
   };
 
   const isImageUrl = (url) => {
@@ -81,6 +129,16 @@ const RedditBrowser = () => {
     <div className="reddit-browser">
       <div className="navbar">
         <h2>Reddit Browser</h2>
+        <div className="move-buttons">
+          {/* Back Button */}
+          <button className="nav-button" onClick={handleBackClick} disabled={!canGoBack}>
+            Back
+          </button>
+          {/* Forward Button */}
+          <button className="nav-button" onClick={handleForwardClick} disabled={!canGoForward}>
+            Forward
+          </button>
+        </div>
         <form onSubmit={handleSubredditSubmit}>
           <div className="search-container">
             <input
